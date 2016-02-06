51d6ff102996d41a0dc986b79bae61b9a53b75a4
maccyber <jonas.enge@gmail.com>
2016-02-06 20:24:22 +0100

    Everything on its right place

--- config/index.js
+++ config/index.js
@@ -6,17 +6,17 @@ var config = {
     orden_group: process.env.ORDEN_GROUP || '/151ORD'
   },
   buddy: {
-    user: process.env.BUDDY_USER || 'tfk-digital', // Database username
-    password: process.env.BUDDY_PASSWORD || 'DigitalSjø42', // Database passord
-    server: process.env.BUDDY_SERVER || 'btvhubfim03.login.top.no', // You can use 'localhost\\instance' to connect to named instance
+    user: process.env.BUDDY_USER || 'username', // Database username
+    password: process.env.BUDDY_PASSWORD || 'password', // Database passord
+    server: process.env.BUDDY_SERVER || 'server.domain.no', // You can use 'localhost\\instance' to connect to named instance
     database: process.env.BUDDY_DATABASE || 'dbMetakatalog', // Database name
     options: {
       encrypt: false // Use this if you're on Windows Azure
     }
   },
   SERVER_PORT: process.env.SERVER_PORT || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || 'Fratres',
-  YAR_SECRET: process.env.JWT_SECRET || 'Fratres'
+  JWT_SECRET: process.env.JWT_SECRET || 'NeverShareYourSecret',
+  YAR_SECRET: process.env.JWT_SECRET || 'NeverShareYourSecret'
 }
 
 module.exports = config
