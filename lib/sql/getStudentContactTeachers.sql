f7451ac2ab7e2b7ef8e5bfafa75dba6f7834cce7
Jonas Enge <jonas.enge@t-fk.no>
2016-08-19 13:07:22 +0200

    Added privateMail

--- lib/sql/getStudentContactTeachers.sql
+++ lib/sql/getStudentContactTeachers.sql
@@ -3,12 +3,14 @@ SET @atGroup = '@atferdGroup';
 DECLARE @user varchar(60);
 SET @user = '@username';
 
-SELECT
-  StringValue as username,
-  LEFT(ID, CHARINDEX('/', ID) - 1)  as groupId
-FROM   dbMetakatalog.dbo.tblMultiValue
-WHERE ID IN
+SELECT 
+  m.StringValue as username,
+  LEFT(m.ID, CHARINDEX('/', m.ID) - 1)  as groupId,
+  o.PrivateMail as mail
+FROM dbMetakatalog.dbo.tblMultiValue m
+  INNER JOIN dbMetakatalog.dbo.tblObjects o ON m.StringValue = o.ID
+WHERE m.ID IN
       (SELECT TOP 1 ID
-        FROM dbMetakatalog.dbo.tblMultiValue
-        WHERE StringValue = @user AND ID LIKE '%' + @atGroup + '%')
-AND AttributeName = 'Owner';
+  FROM dbMetakatalog.dbo.tblMultiValue
+  WHERE StringValue = @user AND ID LIKE '%' + @atGroup + '%')
+  AND m.AttributeName = 'Owner';
